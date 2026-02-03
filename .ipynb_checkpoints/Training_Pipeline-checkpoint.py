c042b3e39a33a611c56e798e1c460dfb5a482e6a
Alejandro <armentaayalaalejandro@gmail.com>
2026-02-03 16:41:41 -0600

    Added more to pipeline

--- .ipynb_checkpoints/Training_Pipeline-checkpoint.py
+++ .ipynb_checkpoints/Training_Pipeline-checkpoint.py
@@ -10,6 +10,7 @@ from sklearn.linear_model import LogisticRegression
 from math import exp
 import pickle
 
+from sqlalchemy import create_engine
 
 #GET STATISTICS
 
@@ -284,8 +285,152 @@ def LogisticRegressionAnalysis():
 
     predict_df.to_csv("Generated/predictions.csv", header=True)
     
+def CreateDataset():
+    DB_USER = 'postgres'
+    DB_PASSWORD = 'postgres'
+    DB_HOST = 'localhost'
+    DB_PORT = '5432'
+    DB_NAME = 'churn'
+    
+    database_url = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    print(database_url)
 
+    sql_query = """set search_path to socialnet7;
+    
+    with observation_params as (
+    select  
+        interval '7 day' as metric_period,
+        '2020-03-01'::timestamp as obs_start,
+        '2020-05-10'::timestamp as obs_end
+        )
+    select 
+        --esta metrica es un churn si o no
+        --esta metrica esta dentro del rango de fechas si o no
+        
+        m.account_id, observation_date, is_churn,
+        --sum(case when metric_name_id = 0 then metric_value else 0 end) as like_per_month,
+        --sum(case when metric_name_id = 1 then metric_value else 0 end) as newfriend_per_month
+    
+        SUM(
+        CASE
+          WHEN metric_name_id = 0 THEN metric_value
+          ELSE 0
+        END
+      ) AS like_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 1 THEN metric_value
+          ELSE 0
+        END
+      ) AS newfriend_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 2 THEN metric_value
+          ELSE 0
+        END
+      ) AS post_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 3 THEN metric_value
+          ELSE 0
+        END
+      ) AS adview_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 4 THEN metric_value
+          ELSE 0
+        END
+      ) AS dislike_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 33 THEN metric_value
+          ELSE 0
+        END
+      ) AS unfriend_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 6 THEN metric_value
+          ELSE 0
+        END
+      ) AS message_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 7 THEN metric_value
+          ELSE 0
+        END
+      ) AS reply_per_month,
+      SUM(
+        CASE
+          WHEN metric_name_id = 21 THEN metric_value
+          ELSE 0
+        END
+      ) AS adview_per_post,
+      SUM(
+        CASE
+          WHEN metric_name_id = 22 THEN metric_value
+          ELSE 0
+        END
+      ) AS reply_per_message,
+      SUM(
+        CASE
+          WHEN metric_name_id = 23 THEN metric_value
+          ELSE 0
+        END
+      ) AS like_per_post,
+      SUM(
+        CASE
+          WHEN metric_name_id = 24 THEN metric_value
+          ELSE 0
+        END
+      ) AS post_per_message,
+      SUM(
+        CASE
+          WHEN metric_name_id = 28 THEN metric_value
+          ELSE 0
+        END
+      ) AS unfriend_per_newfriend,
+      SUM(
+        CASE
+          WHEN metric_name_id = 27 THEN metric_value
+          ELSE 0
+        END
+      ) AS dislike_pcnt,
+      SUM(
+        CASE
+          WHEN metric_name_id = 30 THEN metric_value
+          ELSE 0
+        END
+      ) AS newfriend_pcnt_chng,
+      SUM(
+        CASE
+          WHEN metric_name_id = 31 THEN metric_value
+          ELSE 0
+        END
+      ) AS days_since_newfriend
+        
+        --observation_date
+        --count(m.account_id)
+        
+        from metric m 
+        inner join observation_params on metric_time BETWEEN obs_start and obs_end
+        inner join observation o on m.account_id = o.account_id 
+        and m.metric_time > (o.observation_date - metric_period)::timestamp 
+        and m.metric_time <= o.observation_date::timestamp
+    group by m.account_id, m.metric_time, observation_date, is_churn
+    order by observation_date, m.account_id"""
+    
+    print(sql_query)
 
+    engine = create_engine(database_url)
+    connection = engine.connect()
+    
+    print(connection)
+
+    df = pd.read_sql(sql_query,connection, index_col=['account_id','observation_date'])
+    df.to_csv('Generated/original.csv',header=True)
+    
+
+CreateDataset()
 GenerateStats('Generated/original.csv')
 ScoreData()
 GenerateStats('Generated/scores.csv')
--- .ipynb_checkpoints/build-checkpoint.sh
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-find ./Generated/ -maxdepth 1 -type f ! -name 'original.csv' -delete
+#find ./Generated/ -maxdepth 1 -type f ! -name 'original.csv' -delete
+find ./Generated/ -maxdepth 1 -type f -delete
 
 python Training_Pipeline.py
