e13d54058487fa9b2c840086932fd0806c2bbf42
Alejandro <armentaayalaalejandro@gmail.com>
2026-02-04 15:39:46 -0600

    Made new test

--- .ipynb_checkpoints/build-checkpoint.sh
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -2,6 +2,8 @@
 
 #find ./Generated/ -maxdepth 1 -type f ! -name 'CreateDatabase.sql' ! -name 'CurrentDatabase.sql' -delete
 
-python Training_Pipeline.py ./CreateDatabase.sql ./CurrentDatabase.sql ./MyDatasetIMade
+python Training_Pipeline.py ./myTrain.sql ./myTest.sql ./TestChurn0
+
+python Training_Pipeline.py ./train.sql ./test.sql ./TestChurn1
 
 #python Training_Pipeline.py ./Generated/CreateDatabase.sql ./Generated/CurrentDatabase.sql
